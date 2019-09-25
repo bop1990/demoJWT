69982bb03ee829d6018fa5811774c6fa1c9fe40e
Bruno Petry <bruno.petry@fornecedores.sicoob.com.br>
2019-09-25 17:43:46 -0300

    lombok

--- src/main/java/com/example/demo/security/AccountCredentials.java
+++ src/main/java/com/example/demo/security/AccountCredentials.java
@@ -1,23 +1,9 @@
 package com.example.demo.security;
 
+@Data
 public class AccountCredentials {
 	
 	private String username;
 	private String password;
-
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
+	
 }
